a8991df17faa7943cd98b5c1a87d66b31717669b
Mike M <watkajtys@gmail.com>
2023-10-26 17:06:07 -0600

    new cta component - let's chat and associated styling

---
+++ src/components/izk/cta/cta.tsx
@@ -0,0 +1,19 @@
+import {component$} from "@builder.io/qwik";
+import styles from "./cta.module.css";
+
+export default component$(() => {
+    //ONE CONTAINER DIV WITH TWO CHILDREN EACH TAKING UP 50% OF THE WIDTH
+    return (
+        <div class={styles.grid}>
+            <div class={[styles.grid, styles.flip]}>
+                <div class={['center-center']}><h2 class={'hugetext'}>IZK</h2></div>
+                <div class={[styles.socialcontainer, 'container','container-flex-vertical']}>
+                    <a href="">Social</a>
+                    <a href="">Social</a>
+                    <a href="">Social</a>
+                </div>
+            </div>
+            <div class={['center-center']}><a href="#" class={['hugetext', styles.special]}>Let's Chat</a></div>
+        </div>
+    );
+});
