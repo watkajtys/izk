ee372c209d4f4f15a916296e1a94ae8c76dc8ffc
Mike M <watkajtys@gmail.com>
2023-10-27 17:37:36 -0600

    very basic mobile and desktop pass for the CTA component

--- src/components/izk/cta/cta.tsx
+++ src/components/izk/cta/cta.tsx
@@ -5,7 +5,7 @@ export default component$(() => {
     //ONE CONTAINER DIV WITH TWO CHILDREN EACH TAKING UP 50% OF THE WIDTH
     return (
         <div class={styles.grid}>
-            <div class={[styles.grid, styles.flip]}>
+            <div class={[styles.grid, styles.flip, styles.brand]}>
                 <div class={['center-center']}><h2 class={'hugetext'}>IZK</h2></div>
                 <div class={[styles.socialcontainer, 'container','container-flex-vertical']}>
                     <a href="">Social</a>
