66396f6f1526e2b89089f4b3ab4f19b1c9c05cbf
Mike M <watkajtys@gmail.com>
2023-11-08 13:00:00 -0700

    Adding a TEXT HERO component - this will eventually be reusable

---
+++ src/components/izk/texthero/texthero.tsx
@@ -0,0 +1,14 @@
+import {component$} from "@builder.io/qwik";
+import styles       from "./texthero.module.css";
+
+export default component$(() => {
+    return (
+        <div class={['container', styles.textHeroContainer]}>
+
+            <div class={styles.hugeContainer}>
+                <h1 class={['massivetext']}>WHAT CAN WE DO TOGETHER</h1>
+            </div>
+
+        </div>
+    )
+});
