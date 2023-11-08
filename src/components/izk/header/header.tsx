8f052ff4b17cb6719059ddb70a0697cf60ce7cff
Mike M <watkajtys@gmail.com>
2023-11-08 12:58:02 -0700

    header animations and padding updates

--- src/components/izk/header/header.tsx
+++ src/components/izk/header/header.tsx
@@ -1,6 +1,7 @@
 import {component$} from "@builder.io/qwik";
 import styles from "./header.module.css";
 
+
 export default component$(() => {
    return (
        <header class={styles.header}>
@@ -9,6 +10,7 @@ export default component$(() => {
                <a href="/" title="IZK DIGITAL; WEB DEVELOPMENT IN DENVER">IZK</a>
 
                <div class={styles.menu}>
+                   <a href="/services">What we Do</a>
                    <a href="/">Clients</a>
                    <a href="/">How we work</a>
                </div>
